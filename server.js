d16dcad51cf5803c5c2542fdd848470b1df31ed0
Ziad0120 <164951352+Ziad0120@users.noreply.github.com>
2026-07-03 00:05:26 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ async function spawnWorker(id) {
             
             // بحث ذكي عن مسار الكروم في Render أو أي سيرفر Linux
             // التحقق من المسارات المتاحة لمنع خطأ "executable not found"
-            browser = await puppeteer.launch({
-    executablePath: puppeteer.executablePath(),
+            const browser = await puppeteer.launch({
+    executablePath: "/opt/render/.cache/puppeteer/chrome/linux-127.0.6533.88/chrome-linux64/chrome",
     headless: "new",
     args: [
         "--no-sandbox",
